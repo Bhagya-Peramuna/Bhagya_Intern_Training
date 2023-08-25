40e3c2fdd6cd1a5c744a4a50d097403e9f605e2b
Bhagya-Peramuna <bhagya@qbitum.com>
2023-08-25 16:45:04 +0530

    graphic2d package udated

--- src/face/SmileyFace.java
+++ src/face/SmileyFace.java
@@ -4,15 +4,11 @@ import common.drawer.Circle;
 import common.drawer.Crescent;
 import common.drawer.Shape;
 
-import java.awt.*;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.PathIterator;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class SmileyFace implements java.awt.Shape  {
+//public class SmileyFace implements java.awt.Shape  {
+public class SmileyFace  {
 
     private Collection<Shape>circle = new ArrayList<>();
     private Collection<Shape> crescent = new ArrayList<>();
@@ -38,54 +34,5 @@ public class SmileyFace implements java.awt.Shape  {
         this.crescent = crescent;
     }
 
-    @Override
-    public Rectangle getBounds() {
-        return null;
-    }
-
-    @Override
-    public Rectangle2D getBounds2D() {
-        return null;
-    }
-
-    @Override
-    public boolean contains(double x, double y) {
-        return false;
-    }
-
-    @Override
-    public boolean contains(Point2D p) {
-        return false;
-    }
-
-    @Override
-    public boolean intersects(double x, double y, double w, double h) {
-        return false;
-    }
-
-    @Override
-    public boolean intersects(Rectangle2D r) {
-        return false;
-    }
-
-    @Override
-    public boolean contains(double x, double y, double w, double h) {
-        return false;
-    }
-
-    @Override
-    public boolean contains(Rectangle2D r) {
-        return false;
-    }
-
-    @Override
-    public PathIterator getPathIterator(AffineTransform at) {
-        return null;
-    }
-
-    @Override
-    public PathIterator getPathIterator(AffineTransform at, double flatness) {
-        return null;
-    }
 
 }
